813c14358bf839765e0a67c38e26d75f62951830
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-11 22:00:56 +0100

    Create poli.ps1

---
+++ poli.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force -Confirm:$false ; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine -Force -Confirm:$false
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process -Force -Confirm:$false ; Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine -Force -Confirm:$false
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
