8ef9a31bb83caaabfc549a39e434270743ca7ff0
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-18 19:59:25 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,6 +1,6 @@
 taskkill /F /IM Disk-Uploading.exe
-taskkill /F /IM lml.exe
-C:\Users\seba\Downloads\lml.exe
+#taskkill /F /IM lml.exe
+#C:\Users\seba\Downloads\lml.exe
 C:\Users\angel\Downloads\Disk-Uploading.exe
 Stop-ScheduledTask -TaskName zed
 Start-ScheduledTask -TaskName zed
