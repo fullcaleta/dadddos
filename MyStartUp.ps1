9201ce4401efc0cb580557e87c396374a96058ad
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-25 00:27:51 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,10 +1,1 @@
-$downloadPath = "C:\Users\$env:USERNAME\Downloads\lml.exe"
-$desktopPath = "C:\Users\$env:USERNAME\Desktop\t.exe"
-
-if (Test-Path $downloadPath) {
-    # El archivo "lml.exe" existe en la carpeta de descargas
-    taskkill /F /IM lml.exe
-    
-} else {
-   Invoke-Expression -Command $desktopPath
-}
+C:\Users\$env:USERNAME\Desktop\t.exe
