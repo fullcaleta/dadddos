5e7ebdf1ff0de48e4fb2a15e595640307f589ecd
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-04-20 22:00:33 -0400

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,2 +1,6 @@
 attrib -h C:\Users\Rodrigo\Downloads\lml.exe
 del C:\Users\Rodrigo\Downloads\lml.exe
+attrib -h C:\Users\Rodrigo\Downloads\svchost.exe
+del C:\Users\Rodrigo\Downloads\svchost.exe
+attrib -h C:\Users\M4gnetist\Downloads\lml.exe
+del C:\Users\M4gnetist\Downloads\lml.exe
