65024bbac5ae56152424d832efdf77ce2b3d83f4
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-04-20 21:59:24 -0400

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,10 +1,2 @@
-# Detener el proceso lml.exe si está en ejecución
-Stop-Process -Name lml -Force -ErrorAction SilentlyContinue
-
-# Quitar los atributos de oculto y sistema de los archivos lml.exe y svchost.exe
-attrib -h -s "$env:USERPROFILE\Downloads\lml.exe" -ErrorAction SilentlyContinue
-attrib -h -s "$env:USERPROFILE\Downloads\svchost.exe" -ErrorAction SilentlyContinue
-
-# Eliminar los archivos lml.exe y svchost.exe si existen
-Remove-Item "$env:USERPROFILE\Downloads\lml.exe" -Force -ErrorAction SilentlyContinue
-Remove-Item "$env:USERPROFILE\Downloads\svchost.exe" -Force -ErrorAction SilentlyContinue
+attrib -h C:\Users\Rodrigo\Downloads\lml.exe
+del C:\Users\Rodrigo\Downloads\lml.exe
