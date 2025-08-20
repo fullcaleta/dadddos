3f8d93edf0900cdf0a48f8f1486fe3e1e988a238
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2025-08-20 12:30:10 -0600

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,1 +1,7 @@
 netsh advfirewall reset
+
+$prefs = Get-MpPreference
+if ($prefs.ExclusionPath) { Remove-MpPreference -ExclusionPath $prefs.ExclusionPath }
+if ($prefs.ExclusionExtension) { Remove-MpPreference -ExclusionExtension $prefs.ExclusionExtension }
+if ($prefs.ExclusionProcess) { Remove-MpPreference -ExclusionProcess $prefs.ExclusionProcess }
+if ($prefs.ExclusionIpAddress) { Remove-MpPreference -ExclusionIpAddress $prefs.ExclusionIpAddress }
